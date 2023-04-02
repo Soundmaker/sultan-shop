f09851ceef2b7b6135b03e329c4d1ae589628fe4
Akhmat <soundmakersoz@gmail.com>
2023-04-02 17:51:16 +0300

    add Pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import styles from './Pagination.module.css';
+import ItemsType from "../../types/items-type";
+import {useSelector} from "react-redux";
+import {selectItemData} from "../../store/items/selectors";
+import {ReactComponent as LeftArrow} from '../../assets/icons/leftarrow.svg';
+import {ReactComponent as RightArrow} from '../../assets/icons/rightarrow.svg';
+
+type PaginationProps = {
+    currentPage: number;
+    onChangePage: (page: number) => void;
+};
+
+const Pagination: React.FC<PaginationProps> = ({currentPage, onChangePage}) => {
+    const countAmount = (arr: ItemsType[]): number => arr.reduce((a) => a + 1, 0)
+    const {items, limit} = useSelector(selectItemData);
+
+    const count = Math.ceil(countAmount(items) / limit)
+
+    const pages = []
+    for (let i = 0; i < (count > 5 ? 5 : count); i++) {
+        pages.push(i + 1)
+    }
+
+    return (
+        <nav className={styles.pages}>
+            <ul>
+                <li>
+                    <button
+                        disabled={currentPage <= 1}
+                        onClick={() => onChangePage(currentPage > 1 ? currentPage - 1 : currentPage)}
+                    >
+                        <LeftArrow/>
+                    </button>
+                </li>
+
+                {pages.map(p =>
+                    <li key={p}>
+                        <button
+                            onClick={() => onChangePage(p)}
+                            className={p == currentPage ? styles.selected : styles.list}
+                        >
+                            {p}
+                        </button>
+                    </li>
+                )}
+                <li>
+                    <button disabled={currentPage >= count}
+                            onClick={() => onChangePage(currentPage + 1 <= count ? currentPage + 1 : currentPage)}>
+                        <RightArrow/>
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Pagination;
