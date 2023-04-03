246eef355fa9b48039bea52a803d3703473a1aa2
Akhmat <soundmakersoz@gmail.com>
2023-04-03 11:40:41 +0300

    Updates

--- src/utils/getItemsFromAdmin.ts
+++
@@ -1,8 +0,0 @@
-import json from "../test.json"
-
-export const getItemsFromAdmin = () => {
-    const data = localStorage.getItem('items');
-
-    return data ? (JSON.parse(data).length ? JSON.parse(data) : json) : json
-
-};
